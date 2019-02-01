5b6b6040f1fa07adaa3e64f13a336324240b0914
Muneeb Rehman <mrehman@corp.contoursoftware.net>
2019-02-01 17:21:24 +0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state= {
-      dishes: DISHES
-    };
-  }
-  
-  render() {
+function App() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
-  }
 }
 
 export default App;
